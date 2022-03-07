7b1173f8287cec2957fbca2b684bea90fe8932ec
JM_Asus <rha8565@gmail.com>
2022-03-07 17:42:01 +0900

    test

--- ContentsServer/ServerForm.h
+++ ContentsServer/ServerForm.h
@@ -8,7 +8,7 @@
 namespace ContentsServer 
 {
 
-	using namespace System;
+	using namespace System; 
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
 	using namespace System::Windows::Forms;
