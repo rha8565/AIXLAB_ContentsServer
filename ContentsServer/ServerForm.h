d6473b6ea2590ab3676660dcc4a52149296c38b6
JM_Asus <rha8565@gmail.com>
2022-03-07 17:40:59 +0900

    test

--- ContentsServer/ServerForm.h
+++ ContentsServer/ServerForm.h
@@ -12,7 +12,7 @@ namespace ContentsServer
 	using namespace System::ComponentModel;
 	using namespace System::Collections;
 	using namespace System::Windows::Forms;
-	using namespace System::Data;
+	using namespace System::Data; 
 	using namespace System::Drawing;
 	/// <summary>
 	/// ServerForm에 대한 요약입니다.
